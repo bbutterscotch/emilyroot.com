80845498eba6e2d4707007051947421a5c31a101
eeroot <eeroot@ncsu.edu>
2023-04-21 16:00:49 -0400

    [Project Setup]
    - Created react app
    - Imported bootstrap
    - Deleted unnecessary files

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <h1>Portfolio process...</h1>
+    /**
+    <div className="App">
+      <header className="App-header">
+        <h1>Portfolio process...</h1>
+      </header>
+    </div>
+    */
+  );
+}
+
+export default App;
